3dc90c8af568705d78b78dd8181ee0bd9cef258e
Jędrzej Warczyński <jedrzej.warczynski@student.put.poznan.pl>
2024-12-20 11:47:34 +0100

    dynamically set quantity to monitor in scheduler based on loss

--- src/models.py
+++ src/models.py
@@ -272,6 +272,7 @@ class MOVVAELightning(L.LightningModule):
         return self.optimizers().param_groups[0]["lr"]
 
     def configure_optimizers(self):
+        monitor = "val/binary_ce_recon_loss" if self.loss == "bce" else "val/cross_entropy_recon_loss"
         optimizer = torch.optim.Adam(self.model.parameters(), lr=self.scheduler_params["rlr_initial_lr"])
         scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
             optimizer,
@@ -282,10 +283,10 @@ class MOVVAELightning(L.LightningModule):
         )
         return {
             "optimizer": optimizer,
-            # "lr_scheduler": {
-            #     "scheduler": scheduler,
-            #     "monitor": "val/binary_ce_recon_loss"  # The metric to monitor
-            # }
+            "lr_scheduler": {
+                "scheduler": scheduler,
+                "monitor": monitor  # The metric to monitor
+            }
         }
 
     def on_before_optimizer_step(self, optimizer):
